f17a0c146962115a2ab7ae985e7a9a1dcf4f2be4
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-08-10 18:41:44 +0300

    createAsyncThunk

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchTasks } from './operations';
 
 const tasksInitialState = {
   items: [],
@@ -9,25 +10,22 @@ const tasksInitialState = {
 const tasksSlice = createSlice({
   name: 'tasks',
   initialState: tasksInitialState,
-  reducers: {
-    // Виконається в момент старту HTTP-запиту
-    fetchingInProgress(state, action) {
-      state.isLoading = true;
-    },
-    // Виконається якщо HTTP-запит завершився успішно
-    fetchingSuccess(state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    // Виконається якщо HTTP-запит завершився з помилкою
-    fetchingError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+  // Додаємо обробку зовнішніх екшенів
+  extraReducers: builder => {
+    builder
+      .addCase(fetchTasks.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
-export const { fetchingInProgress, fetchingSuccess, fetchingError } =
-  tasksSlice.actions;
 export const tasksReducer = tasksSlice.reducer;
